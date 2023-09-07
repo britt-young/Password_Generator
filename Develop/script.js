88ab0d4f0194529259291d1f59942c428e18e066
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-09-07 13:37:29 -0400

    added constant variables into index.html and javascript events into js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,28 @@
 // Assignment code below
+
+// initialize global variables??
+
 // create an array of possible user input characters
 var charOptions = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]","^", "_", "`", "{", "|", "}", "~"];
 
+// event on button element
+const btn = document.querySelector("button");
 
+// create a function for generation
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
 
+// when button is clicked (event occured), randomly select password combo of ## character
+function generatePass() {
+  const newPass = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.getElementById("password") = newPass;
+}
 
-
-
-
-
-
+btn.addEventListener("click", generatePass);
 
 // Assignment code above
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
