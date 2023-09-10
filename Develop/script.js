944c4442fd7720f3aa5a7d9394e8d5bc9e3744eb
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-09-10 10:42:29 -0400

    added click event in html and  restructured js order

--- Develop/script.js
+++ Develop/script.js
@@ -1,62 +1,17 @@
 // Assignment code below
-var button = document.querySelector("btn");
+var button = document.querySelector("button");
 document.addEventListener("click", passCriteria() );
 
+var passwordBox = document.getElementById("password");
+var length = 12;
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var num = "0123456789";
 // var symbol = " !@#$%^&*()"'*+,-./;:<=>?{}[]_`~ ";
 var allChar = upperCase + lowerCase + num + symbol;
 
-function passCriteria(){
-  //uppercase criteria prompt
-  var userUpperInput = window.prompt("Do you want to include upper case letters? Y or N?")
-  // if user inputs a lowercase character, change to uppercase
-  if (userUpperInput.toUpperCase() === "Y") {
-   //***confirm yes and "track" user response***
-   window.alert("👍")
-  }
-  else {
-    window.alert("Password MUST contain atleast 1 uppercase letter");
-  }
-
-//lowercase criteria prompt
-  var userLowerInput = window.prompt("Do you want to include lowercase letters? Y or N?")
-  // if user inputs a lowercase character, change to uppercase
-  if (userLowerInput.toUpperCase() === "Y") {
-    window.alert("👍")
-   //***confirm yes and "track" user response***
-  }
-  else {
-    window.alert("Password MUST contain atleast 1 lowercase letter");
-  }
-
-//numerical criteria prompt
-  var userNumInput = window.prompt("Do you want to include numbers? Y or N?")
-  // if user inputs a lowercase character, change to uppercase
-  if (userNumInput.toUpperCase() === "Y") {
-    window.alert("👍")
-   //***confirm yes and "track" user response***
-  }
-  else {
-    window.alert("Password MUST contain atleast one number 0-9");
-  }
-
-//special characters criteria prompt
-  var userSpecInput = window.prompt("Do you want to include special characters? Y or N?")
-  // if user inputs a lowercase character, change to uppercase
-  if (userSpecInput.toUpperCase() === "Y") {
-    window.alert("👍")
-   //***confirm yes and "track" user response***
-  }
-  else {
-    window.alert("Password MUST contain atleast 1 special character");
-  }
-}
-
-
 // create a function to generate random password using at least (1) uppercase (1) lowercase (1) number and (1) symbol
-/* function generatePass() {
+function generatePass() {
   password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
@@ -69,10 +24,66 @@ function passCriteria(){
     password += allChar[Math.floor(Math.random() * allChar.length)];
   }
   passwordBox.value = password;
-} */
+} 
+
+// function to generate criteria prompts then execute password generation
+function passCriteria(){
+  //uppercase criteria prompt
+  var userUpperInput = window.prompt("Do you want to include uppercase letters? Y or N?")
+  // if user inputs a lowercase character, change to uppercase
+  if (userUpperInput.toUpperCase() === "Y") {
+   //***confirm yes and "track" user response***
+   window.alert("👍")
+  }
+  else {
+    window.alert("Password MUST contain atleast 1 uppercase letter");
+  }
+
+
+//lowercase criteria prompt
+  var userLowerInput = window.prompt("Do you want to include lowercase letters? Y or N?")
+  // if user inputs a lowercase character, change to uppercase
+  if (userLowerInput.toUpperCase() === "Y") {
+    window.alert("👍")
+   //***confirm yes and "track" user response***
+  }
+  else {
+    window.alert("Password MUST contain atleast 1 lowercase letter");
+  }
+
+
+//numerical criteria prompt
+  var userNumInput = window.prompt("Do you want to include numbers? Y or N?")
+  // if user inputs a lowercase character, change to uppercase
+  if (userNumInput.toUpperCase() === "Y") {
+    window.alert("👍")
+   //***confirm yes and "track" user response***
+  }
+  else {
+    window.alert("Password MUST contain atleast one number 0-9");
+  }
+
+
+//special characters criteria prompt
+  var userSpecInput = window.prompt("Do you want to include special characters? Y or N?")
+  // if user inputs a lowercase character, change to uppercase
+  if (userSpecInput.toUpperCase() === "Y") {
+    window.alert("👍")
+   //***confirm yes and "track" user response***
+  }
+  else {
+    window.alert("Password MUST contain atleast 1 special character");
+  }
+
+// run generatePass function to create password
+  generatePass()
+}
 
 // Assignment code above
 
+
+
+/* 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -86,3 +97,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+ */
