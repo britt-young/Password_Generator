a64efbdeba5be1727744b00f8f39fec49f895d94
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-09-10 11:36:44 -0400

    tutor suggestions: created new function to tie in exisiting code to newly written generatePass() and passCriteria() functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 // Assignment code below
-var button = document.querySelector("button");
-document.addEventListener("click", passCriteria() );
+/* var button = document.querySelector("button");
+document.addEventListener("click", passCriteria() ); */
 
 var passwordBox = document.getElementById("password");
 var length = 12;
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var num = "0123456789";
-// var symbol = " !@#$%^&*()"'*+,-./;:<=>?{}[]_`~ ";
+var symbol = " !@#$%^&*()'*+,-./;:<=>?{}[]_`~ ";
 var allChar = upperCase + lowerCase + num + symbol;
 
 // create a function to generate random password using at least (1) uppercase (1) lowercase (1) number and (1) symbol
@@ -20,10 +20,10 @@ function generatePass() {
 
   //because we want a password longer than (4) characters (((1) uppercase (1) lowercase (1) number and (1) symbol)) we need to add an if conditional statement to randomly select more characters
   // if the current length is less than the desired password.length
-  if (length > password.length) {
+  for(let i = 0; i < length; i++){
     password += allChar[Math.floor(Math.random() * allChar.length)];
   }
-  passwordBox.value = password;
+ return password;
 } 
 
 // function to generate criteria prompts then execute password generation
@@ -31,59 +31,73 @@ function passCriteria(){
   //uppercase criteria prompt
   var userUpperInput = prompt("Do you want to include uppercase letters? Y or N?")
   // if user inputs a lowercase character, change to uppercase
-  if (userUpperInput.toUpperCase() === "Y") {
+  if (userUpperInput === "Y") {
    //***confirm yes and "track" user response***
    window.alert("👍")
   }
   else {
     window.alert("Password MUST contain atleast 1 uppercase letter");
+    break;
   }
 
 
 //lowercase criteria prompt
   var userLowerInput = prompt("Do you want to include lowercase letters? Y or N?")
   // if user inputs a lowercase character, change to uppercase
-  if (userLowerInput.toUpperCase() === "Y") {
+  if (userLowerInput === "Y") {
     window.alert("👍")
    //***confirm yes and "track" user response***
   }
   else {
     window.alert("Password MUST contain atleast 1 lowercase letter");
+    break;
   }
 
 
 //numerical criteria prompt
   var userNumInput = prompt("Do you want to include numbers? Y or N?")
   // if user inputs a lowercase character, change to uppercase
-  if (userNumInput.toUpperCase() === "Y") {
+  if (userNumInput === "Y") {
     window.alert("👍")
    //***confirm yes and "track" user response***
   }
   else {
     window.alert("Password MUST contain atleast one number 0-9");
+    break;
   }
 
 
 //special characters criteria prompt
-  var userSpecInput = prompt("Do you want to include special characters? Y or N?")
+  var userSymbInput = prompt("Do you want to include special characters? Y or N?")
   // if user inputs a lowercase character, change to uppercase
-  if (userSpecInput.toUpperCase() === "Y") {
+  if (userSymbInput === "Y") {
     window.alert("👍")
-   //***confirm yes and "track" user response***
+      //***confirm yes and "track" user response***
   }
   else {
     window.alert("Password MUST contain atleast 1 special character");
+    break;
   }
 
-// run generatePass function to create password
-  generatePass()
+  // if no select, then alert
+  // if userNumbInput true, addd numb into allChar.
+  // 
+  //
+  // 
+  
 }
 
+function generatePassword(){
+  passCriteria();
+  var resultPass = generatePass();
+  return resultPass;
+
+}
 // Assignment code above
 
 
 
-/* 
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -97,4 +111,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- */
