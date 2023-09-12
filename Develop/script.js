92cfaee54a20de8ce2406ab77c9281410921934b
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-09-12 00:55:51 -0400

    refined generatePassword function and passCriteria function

--- Develop/script.js
+++ Develop/script.js
@@ -7,55 +7,53 @@ var num = "0123456789";
 var symbol = " !@#$%^&*()'*+,-./;:<=>?{}[]_`~ ";
 
 //variables dependent on user input
-var length = "";
 var allChar = upperCase + lowerCase + num + symbol;
-
 
 // function to generate criteria prompts then execute password generation
 function passCriteria() {
   //length prompt
   var userLengthInput = prompt("How long do you want your password to be? (12-128)");
-  if (userLengthInput < 12 | userLengthInput > 128) {
-    return;
-  }
-  userLengthInput.length = length;
+      if (userLengthInput < 12 || userLengthInput > 128) {
+      alert("password must be between 12-128 characters!");
+      return;
+    }
 
   //uppercase criteria prompt
   var userUpperInput = confirm("Include uppercase letters in your password?");
-  if (userUpperInput === "Y") {
-  // include in allChars variable
-    
-  } else {
-    //don't include
-  }
-
+/*   if (userUpperInput === true) {
+    // include in allChars variable
+  
+    } else {
+      //don't include
+    } */
+  
   //lowercase criteria prompt
-  var userLowerInput = confirm("Include lowercase letters in your password?");
-  if (userLowerInput === "Y") {
-// include in allChars variable
-
-  } else {
-    //don't include
-  }
-
+ var userLowerInput = confirm("Include lowercase letters in your password?");
+ /*  if (userLowerInput === true) {
+    // include in allChars variable
+  
+    } else {
+      //don't include
+    } */
+  
   //numerical criteria prompt
   var userNumInput = confirm("Include numbers in your password?");
-  if (userNumInput === "Y") {
-  // include in allChars variable
-
-  } else {
-    //don't include
-  }
-
+ /*  if (userNumInput === true) {
+    // include in allChars variable
+  
+    } else {
+      //don't include
+    } */
+  
   //special characters criteria prompt
-  var userSymbInput = confirm("Include special characters in your password?");
-  if (userSymbInput === "Y") {
-// include in allChars variable
-
-  } else {
-    //don't include
-  }
-
+ var userSymbInput = confirm("Include special characters in your password?");
+   /*  if (userSymbInput === true) {
+    // include in allChars variable
+  
+    } else {
+      //don't include
+    } */
+}
 
 // create a function to generate random password using user selected criteria
 function generatePass() {
@@ -71,14 +69,6 @@ function generatePass() {
     password += allChar[Math.floor(Math.random() * allChar.length)];
   }
   return password;
-}
-
-
-  // if no select, then alert
-  // if userNumbInput true, addd numb into allChar.
-  //
-  //
-  //
 }
 
 function generatePassword() {
@@ -99,5 +89,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Add event listener to writePassword upon button click
 generateBtn.addEventListener("click", writePassword);
